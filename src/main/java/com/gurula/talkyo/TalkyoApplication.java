619518b45654a95943c42dc5710e2081c3fbd3ea
yifan <yifanchen0914@gmail.com>
2025-01-14 14:54:49 +0800

    0114 YiFan: 創立英文口說AI專案

---
+++ src/main/java/com/gurula/talkyo/TalkyoApplication.java
@@ -0,0 +1,13 @@
+package com.gurula.talkyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkyoApplication.class, args);
+	}
+
+}
